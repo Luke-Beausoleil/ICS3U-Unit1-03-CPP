9ede168fb44468df7c22c7ea3107e4007678fa8c
Luke Beausoleil <luke.beausoleil@mths.ca>
2021-04-23 15:37:50 +0000

    added helloWorld program with proper format

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Luke Beausoleil All rights reserved
+//
+// Created by: Luke Beausoleil
+// Created on: April 2021
+// This is a "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
